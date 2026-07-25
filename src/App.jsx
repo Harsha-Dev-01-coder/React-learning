8640ededcd3144a7ca8602e90e5b792a019bb11a
Harsha-Dev-01-coder <j.harshavardhan024@gmail.com>
2026-07-25 14:35:15 +0530

    Added Components and Props, JSX Expressions and Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
-import Greeting from "./Greeting";
+import Greeting from "./components/Greeting";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Student from "./components/Student";
+import Card from "./components/Card"
+import Product from "./components/Product"
+import Footer from "./components/Footer";
 
 function App() {
-  return <Greeting />;
+  const name = "Champ";
+  const isLoggedIn = true;
+  const marks = 85;
+  const isPremium = true;
+  const fruits = [
+    "Apple",
+    "Mango",
+    "Orange"
+  ];
+
+  return (
+    <>
+      <h1>
+        {isLoggedIn ? "Welcome to" : "Please Login"}
+      </h1>
+      <>
+        {isPremium && <h1>Premium User</h1>}
+      </>
+      <Header />
+      <Greeting />
+      <Main />
+      <Student
+        name="Champ" 
+        course="Frontend"
+      />
+        <>
+          <Card title="HTML" />
+          <Card title="CSS" />
+          <Card title="JavaScript" />
+          <Card title="React" />
+        </>
+        <>
+          <Product name="Keyboard" price="1999" />
+          <Product name="Mouse" price="999" />
+        </>
+        <h1>Hello {name.toUpperCase}</h1>
+        <h2>
+          {marks <= 85 ? "Pass" : "Fail"}
+        </h2>
+        <>
+          {fruits.map((fruit, index) => (
+            <h2 key={index}>{fruit}</h2>
+          ))}
+        </>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Student.jsx
@@ -0,0 +1,10 @@
+function Student({ name, course }) {
+    return (
+    <>
+        <h2>Hey {name}</h2>
+        <p>{course}</p>
+    </>
+    );
+}
+
+export default Student;
